1f6e9e123d7163080dbdfeb798836f62129d892c
lutfir2019 <lutfirrahman2019@gmail.com>
2024-09-10 11:08:12 +0700

    fix google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,23 @@ const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 export const metadata: Metadata = {
   title: "Khodam Checker",
   description: "Cek Khodam anda dan miliki teman pendamping",
-  keywords: ["mystic", "horor", "khodam", "cek khodam", "check", "check khodam", "cek kodam", "vercel", "nextjs", "react", "jin", "pendamping", "makhluk", "makhluk halus", "jin pendamping"],
+  keywords: [
+    "mystic",
+    "horor",
+    "khodam",
+    "cek khodam",
+    "check",
+    "check khodam",
+    "cek kodam",
+    "vercel",
+    "nextjs",
+    "react",
+    "jin",
+    "pendamping",
+    "makhluk",
+    "makhluk halus",
+    "jin pendamping",
+  ],
 };
 
 interface Root {
@@ -21,18 +37,30 @@ export default function RootLayout({ children }: Readonly<Root>) {
     <html lang="en">
       <head>
         {/* Google Tag Manager */}
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5SZS6JW4');</script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5SZS6JW4');
+            `,
+          }}
+        />
         {/* End Google Tag Manager */}
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
       </head>
       <body className={roboto.className}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SZS6JW4"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5SZS6JW4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
