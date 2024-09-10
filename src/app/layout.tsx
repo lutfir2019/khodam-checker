1bc37725c45f8d3363625216d8271efe7a7dc173
Lutfir Rahman <130606108+lutfir2019@users.noreply.github.com>
2024-09-10 11:00:12 +0700

    google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,20 @@ export default function RootLayout({ children }: Readonly<Root>) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5SZS6JW4');</script>
+        {/* End Google Tag Manager */}
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
       </head>
       <body className={roboto.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SZS6JW4"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
         <LoadingPage />
